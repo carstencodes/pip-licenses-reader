966540097a22094a3ffc97df0d7d297465057095
Carsten Igel <cig@bite-that-bit.de>
2021-01-17 18:31:38 +0100

    "Bumped version from 0.8.0 to 0.9.0"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ copyright = "2020, Carsten Igel"
 author = "Carsten Igel"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
